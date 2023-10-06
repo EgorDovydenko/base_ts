8eb755bcdb450e85cce027e5af96662c67e6bb75
YegorDovydenko <dovydenko.e93@gmail.com>
2023-10-06 19:51:43 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -14,17 +15,30 @@ module.exports = {
     rules: [
       {
         test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
         loader: "ts-loader",
       },
-      {
-        test: /\.css$/,
-        loader: "css-loader",
+      { 
+        test: /\.css$/, 
+        // exclude: /node_modules/,
+        use: [ 
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+        ] 
       },
+      { 
+        enforce: "pre", 
+        test: /\.js$/, 
+        loader: "source-map-loader" 
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, "src", "index.html"),
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
     }),
   ],
 }
